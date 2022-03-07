f4264d37d2377cea7e57393ff9bafa027548f034
Skylar Kelty <skylarkelty@gmail.com>
2022-03-07 12:09:48 +0000

    These are handled by Python

--- docker/cinder/cinder-volume/extend_start.sh
+++ docker/cinder/cinder-volume/extend_start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ $(stat -c %U:%G /var/lib/cinder) != "cinder:kolla" ]]; then
-    sudo chown -R cinder:kolla /var/lib/cinder
-fi
-if [[ $(stat -c %a /var/lib/cinder) != "2775" ]]; then
-    sudo chmod 2775 /var/lib/cinder
-fi
 if [[ $(stat -c %U:%G /etc/iscsi) != "cinder:kolla" ]]; then
     sudo chown -R cinder:kolla /etc/iscsi
 fi
