9afc382777a70534fa2886a6a00d71995ebec978
Skylar Kelty <skylarkelty@gmail.com>
2022-03-07 10:05:22 +0000

    Capture PermissionsErrors too

--- docker/base/set_configs.py
+++ docker/base/set_configs.py
@@ -347,7 +347,7 @@ def handle_permissions(config):
 
             try:
                 os.chown(path, uid, gid)
-            except OSError:
+            except:
                 LOG.exception('Failed to change ownership of %s to %s:%s',
                               path, uid, gid)
 
@@ -369,7 +369,7 @@ def handle_permissions(config):
 
                 try:
                     os.chmod(path, perm)
-                except OSError:
+                except:
                     LOG.exception('Failed to set permission of %s to %s',
                                   path, perm)
 
